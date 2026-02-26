d8427c7f38504e8a2f75d5e40da269b5dcd32326
hailey-7113 <73400331+hailey-7113@users.noreply.github.com>
2026-02-26 17:33:25 -0600

    added library system class

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -1,3 +1,25 @@
+ import java.util.ArrayList;
+
 public class LibrarySystem {
+    static ArrayList<Book> inventory = new ArrayList<>();
+     Book book1 = new Book("Java Basics", "Person", "123-456");
+     inventory.add(book1);
+
     
+    public static void borrow(Book studentBook) {
+        String title = studentBook.getTitle();
+
+        for (Book book : inventory) {
+            if (book.getTitle().equals(title)) {
+                if (book.isAvailable()) {
+                    //change
+                    studentBook.setAvailablility(false);
+                    System.out.println(student.getName() + " borrowed " + title + ".");
+
+                } else {
+                    System.out.println(title + " is not available.");
+                }
+            }
+        }
+    } 
 }
