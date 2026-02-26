ce3add65207abb481b312807612413bf42690f47
Casen Parmer <101528166+casen-parmer@users.noreply.github.com>
2026-02-26 17:33:16 -0600

    Implement main function

--- src/App.java
+++ src/App.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LibrarySystem sys = new LibrarySystem();
+        Scanner s = new Scanner(System.in);
+        String name = null;
+        int studentID = 0;
+        while (!(sys.studentSearch(name, studentID) != null)) {
+            System.out.print("Name: ");
+            name = s.nextLine();
+            System.out.print("Student ID: ");
+            studentID = s.nextInt();
+        }
+        s.nextLine();
+        String cmd = null;
+        while (!(cmd.equalsIgnoreCase("exit"))) {
+            System.out.print(">");
+            cmd = s.nextLine();
+            switch(cmd) {
+                case "help" :
+                    System.out.println("help - prints a list of helpful commands");
+                    System.out.println("exit - exits program");
+                    System.out.println("list - lists books currently checked out by user");
+                    System.out.println("checkout - checkout book");
+                    System.out.println("return - return book");
+                    break;
+                case "exit" :
+                    break;
+                case "list" :
+                    break;
+                case "checkout" :
+                    break;
+                case "return" :
+                    break;
+                default:
+                    System.out.println("Unrecognized command");
+            }
+        }
+
     }
 }
